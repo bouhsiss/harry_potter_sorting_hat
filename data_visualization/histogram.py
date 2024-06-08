88af09474b8282faeba0ca733d6b78874f719b4b
bouhsiss <bouhsisshouda@gmail.com>
2024-06-08 17:50:00 +0100

    [DSLR] : added interpretation

--- data_visualization/histogram.py
+++ data_visualization/histogram.py
@@ -32,4 +32,15 @@ for i, course in enumerate(courses, 1):
     plt.legend()
 
 plt.tight_layout()
-plt.show()    
+plt.show()
+
+# interpretation
+# to identify the most homogenous distribution score we should take in consideration these factors among all four houses:
+# shape consistency (the shape is consistent across all houses) - range and spread is similar across the houses
+# central tedency (mean and median) the more these values are similar across all houses the more homogenous the distribution
+# overlapping areas (more overlap indicates more similarity in distributions)
+
+# after plotting our data we observe that there are two courses that checks these criterias : 
+# -- Arithmancy and care of magical creatures -- 
+# but there are some differences, care of magical creatures shows more homogeneity. the narrow range, aligned peaks
+# and high degree of overlap suggest that the score distributions are more similar across the houses compared to arithmancy which has a wider range and spread and some outliers 
